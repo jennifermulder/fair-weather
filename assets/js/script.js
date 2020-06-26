16c4bf4fb682e0d817dd207a6f0f5942bac3a30a
J Mulder <jlmulder2014@gmail.com>
2020-06-26 15:45:21 -0700

    saving city searches to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,21 @@
 var searchButtonEl = document.querySelector("#search-button");
 var nameInputEl = document.querySelector("#cityname");
+var weatherButtonContainerEl = document.querySelector("#weather-buttons");
 var citySearchEl = document.querySelector("#city-search");
 var currentCityWeatherEl = document.querySelector("#current-city-weather");
+var fiveDayCityContainerEl = document.querySelector("#fiveday-city-container");
 var fiveDayCityWeatherEl = document.querySelector("#fiveday-city-weather");
 
 
 //passing cityName parameter into the function so that it can be searched
 var getCurrentWeather = function (cityName) {
-    var apiCurrentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=a514515ab34188949832b8c89e71bb2e";
+    var apiCurrentUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid&appid=a514515ab34188949832b8c89e71bb2e";
 
     fetch(apiCurrentUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    
+                    getUVIndex(data.coord.lat, data.coord.lon);
                     displayCurrentWeather(data, cityName);
                 });
             } else {
@@ -26,8 +28,27 @@ var getCurrentWeather = function (cityName) {
         });
 };
 
+var getUVIndex = function (lat, lon) {
+    var apiUVIUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=a514515ab34188949832b8c89e71bb2e&lat=" + lat + "&lon=" + lon;
+
+    fetch(apiUVIUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {                    
+                    displayCurrentWeather(data.uv);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        // incase theres any issues with the request
+        .catch(function (error) {
+            alert("Unable to connect to Weather UV Index");
+        });
+};
+
 var getFiveDayWeather = function (cityName) {
-    var apiFiveDayUrl = "https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=a514515ab34188949832b8c89e71bb2e";
+    var apiFiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid&appid=a514515ab34188949832b8c89e71bb2e";
 
     fetch(apiFiveDayUrl)
         .then(function (response) {
@@ -55,9 +76,12 @@ var formSubmitHandler = function (event) {
     if (cityName) {
         getCurrentWeather(cityName);
         getFiveDayWeather(cityName);
+        saveCityName(cityName);
 
         //to clear the form 
         nameInputEl.value = "";
+
+        // displayCitySearch();
     } else {
         alert("Please enter a valid city name");
     }
@@ -74,6 +98,8 @@ console.log(date);
 
 //display the responses/ create elements ( how to pull in parameters)
 var displayCurrentWeather = function (data, cityName) {
+    
+    //let icon = data.weather[0].icon
     let createHtml = function(temp, humidity, windSpeed, uv) {
         let html = `<p class="current-city">${cityName} (<span id="currentDay">${date}</span>) <span id="currentIcon"></span></p>
     <div id="current-temperature">${temp}</div>
@@ -107,8 +133,8 @@ var displayCurrentWeather = function (data, cityName) {
 
     //append children
     currentNameDateIconEl.appendChild(iconEl);
-    currentCityWeatherEl.innerHTML = createHtml("Temperature: " + data.main.temp + " F", '40%', '40mph', '10');//.appendChild(currentNameDateIconEl);
-
+    currentCityWeatherEl.innerHTML = createHtml("Temperature: " + data.main.temp + " •F", "Humidity: " + data.main.humidity + "%", "Wind Speed: " + data.wind.speed + " MPH", '10');
+    
     //create stats ex:temperature div
     var currentTemperatureEl = document.createElement("div");
    // currentTemperatureEl.innerHTML("Temperature: " + data.list[0].main.temp + " F");
@@ -128,9 +154,16 @@ var displayCurrentWeather = function (data, cityName) {
 var displayFiveDayWeather = function (data, cityName) {
     console.log(data)
     console.log(cityName)
+
+    //create date element
+    var forecastTextEl = document.createElement("p");
+    forecastTextEl.textContent = "5-Day Forecast:";
+    forecastTextEl.className = "five-day"
+    fiveDayCityContainerEl.appendChild(forecastTextEl);
+
     let createHTML = function(date, temp, humidity) {
         let html = `
-                <div id="five-date">
+                 <div id="five-date">
                     ${date}
                  </div>
                  <div id="five-icon">
@@ -146,11 +179,15 @@ var displayFiveDayWeather = function (data, cityName) {
     }
     //create 5 day forecast
 
-    for (i = 0; i < 6; i++) {
+    for (i = 0; i < 5; i++) {
         let d = data.list[i];
+
+        
         let cardDiv = document.createElement('div')
-        cardDiv.classList.add('card');
-        cardDiv.innerHTML = createHTML(d.dt_txt, "Temp: " + d.main.temp + " F", "Humidity: " + d.main.humidity + "%")
+        cardDiv.classList.add('card', 'mx-2');
+        // let date = new date d.dt_txt.text.format("MM/DD/YYYY");
+        // d.dt_txt = new d.dt_txt.format("MM/DD/YYYY");
+        cardDiv.innerHTML = createHTML(d.dt_txt, "Temp: " + d.main.temp + " •F", "Humidity: " + d.main.humidity + "%")
         fiveDayCityWeatherEl.append(cardDiv)
         //create 5 cards
        /* var weatherCardEl = document.createElement("div");
@@ -182,36 +219,76 @@ var displayFiveDayWeather = function (data, cityName) {
 
 
 
-// //display search results from saved in local storage (get item, append to element created) DISPLAY AS BUTTON
+//display search results from saved in local storage (get item, append to element created) DISPLAY AS BUTTON
 // var displayCitySearch = function () {
     
 //     var weatherButtonsEl = document.createElement("button");
 //     weatherButtonsEl.textContent = cityName;
 // }
 
-// //function to save search results in local storage
-// var saveSearch = function () {
-
-// }
 
 
-// //looks for event (button click event listener below, when you click on a saved weather button)
-// var buttonClickHandler = function(event) {
-//     //identifies the target of the event (what was clicked on) and gets value of that "data-city-name" attribute
-//     var cityName = event.target.getAttribute("data-city-name")
-//     //value that is retrieved
-//     //console.log(language);
-//     if (cityName) {
-//         //pass this specified language into the getFeaturedRepos function (use as input)
-//         getCurrentWeather(cityName);
-//         getFiveDayWeather(cityName);
+//function to save search results in local storage
+var saveCityName = function(cityName) {
+    var city = {
+        name: cityName
+    };
+    storeCityName(city);
+}
 
-//         // clear old content (clears it first - asynchronous)
-//         currentCityWeatherEl.textContent = "";
-//         fiveDayCityWeatherEl.textContent = "";
-//       }
-// }
+//save task array to local storage
+function storeCityName(city) {
+    //find any items currently saved in local storage
+    var cities = JSON.parse(localStorage.getItem("cities"));
+    if (!cities) {
+        cities = [];
+    }
+
+    cities.push(city);
+
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+//on page load, pull from storage or create a blank array
+function loadCities() {
+    var cities = JSON.parse(localStorage.getItem("cities")) || [];
+    // go through time collection, for every element in time collection find every element to be populated with local stoage data 
+    // loop through collection
+    for (var i = 0; i < cities.length; i++) {
+        // grab the task at each index
+        var city = cities[i];
+        console.log(city);
+
+    var weatherButtonEl = document.createElement("div");
+    console.log(weatherButtonEl);
+    
+    weatherButtonEl.textContent = city;
+    console.log(city);
+    
+    weatherButtonEl.setAttribute("data-city-name", city);
+    weatherButtonContainerEl.appendChild(weatherButtonEl);
+    }
+}
+
+
+
+//looks for event (button click event listener below, when you click on a saved weather button)
+var buttonClickHandler = function(event) {
+    //identifies the target of the event (what was clicked on) and gets value of that "data-city-name" attribute
+    var cityName = event.target.getAttribute("data-city-name")
+    //value that is retrieved
+    //console.log(language);
+    if (cityName) {
+        //pass this specified language into the getFeaturedRepos function (use as input)
+        getCurrentWeather(cityName);
+        getFiveDayWeather(cityName);
+
+        // clear old content (clears it first - asynchronous)
+        currentCityWeatherEl.textContent = "";
+        fiveDayCityWeatherEl.textContent = "";
+      }
+}
 
 searchButtonEl.addEventListener("click", formSubmitHandler);
 
-// weatherButtonsEl.addEventListener("click", buttonClickHandler);
+// weatherButtonEl.addEventListener("click", buttonClickHandler);
